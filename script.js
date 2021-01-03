1974d1c8e7894d0866d5722f5f4d3d6068b8c686
kelvinhovinho <kelvinadamba@hotmail.com>
2021-01-04 00:20:33 +0300

    reverse of clicking the paragraph on about us

--- script.js
+++ script.js
@@ -14,12 +14,24 @@ $(document).ready(function(event){
         document.querySelector("#image1").style.display = "none";
         document.querySelector("#design").style.display = "block";
       });
+      document.querySelector("#design").addEventListener("click", function() {
+        document.querySelector("#design").style.display = "none";
+        document.querySelector("#image1").style.display = "block";
+      });
     document.getElementById("image2").addEventListener("click", function() {
         document.querySelector("#image2").style.display = "none";
         document.querySelector("#development").style.display = "block";
+      });
+    document.querySelector("#development").addEventListener("click", function() {
+        document.querySelector("#development").style.display = "none";
+        document.querySelector("#image2").style.display = "block";
       });
     document.getElementById('image3').addEventListener('click',function(){
         document.querySelector('#image3').style.display='none';
         document.querySelector('#product').style.display='block';
     });
+    document.querySelector("#product").addEventListener("click", function() {
+        document.querySelector("#product").style.display = "none";
+        document.querySelector("#image3").style.display = "block";
+      });
 });
