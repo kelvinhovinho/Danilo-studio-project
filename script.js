40bf3b53d302249908f3f106ce5acc03a2f149ff
kelvinhovinho <kelvinadamba@hotmail.com>
2021-01-03 19:09:33 +0300

    add the submit alert

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(event){
+    $('.btn').click(function(){
+        var name =$('#name').val();
+        if(name==""){
+            alert("please provide a name");
+        }else{
+            alert(`${name} we have received your message. Thank you for reaching out to us.`);
+        }
+
+        
+    });  
+});
